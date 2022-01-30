461f06a7dbf65db6836c74090e68549600380c28
Aman garg <aman0944.cse19@chitkara.edu.in>
2022-01-31 02:17:31 +0530

    Updates

---
+++ precache-manifest.72428217d5394167e105c1c384ae4437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06da619e3c8a014df241daa15d11bd14",
+    "url": "/Food_recipe/index.html"
+  },
+  {
+    "revision": "5d61087e6a5fcbfcea75",
+    "url": "/Food_recipe/static/css/2.c79bc260.chunk.css"
+  },
+  {
+    "revision": "2db6cd75ef9618e76150",
+    "url": "/Food_recipe/static/css/main.23fb50a6.chunk.css"
+  },
+  {
+    "revision": "5d61087e6a5fcbfcea75",
+    "url": "/Food_recipe/static/js/2.18e0e818.chunk.js"
+  },
+  {
+    "revision": "b2a5d1fb403c14173016b134311c7045",
+    "url": "/Food_recipe/static/js/2.18e0e818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db6cd75ef9618e76150",
+    "url": "/Food_recipe/static/js/main.671ea021.chunk.js"
+  },
+  {
+    "revision": "29b923f52223a1871dad",
+    "url": "/Food_recipe/static/js/runtime-main.955170e1.js"
+  }
+]);
